8aaac6399b81ab9bef7ae7d1095a0eb4335a29aa
houshmandX <houshmand.esmaeilpour@gmail.com>
2021-04-12 21:31:29 -0700

    changed resume

--- doc.js
+++ doc.js
@@ -1,5 +1,13 @@
-function myFunction() {
+function myAbout() {
   var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
+function myCources() {
+  var x = document.getElementById("mycource");
   if (x.style.display === "none") {
     x.style.display = "block";
   } else {
